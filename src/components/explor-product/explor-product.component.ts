3a80c538ae277875bd919058322bd0fcd017bedb
Sedal <fanavinai2@gmail.com>
2024-08-28 14:53:42 +0330

    explor-finish project

--- src/components/explor-product/explor-product.component.ts
+++ src/components/explor-product/explor-product.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component ,Input} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {BotenComponent} from "../boten/boten.component";
 
 @Component({
   selector: 'app-explor-product',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    BotenComponent
+  ],
   templateUrl: './explor-product.component.html',
   styleUrl: './explor-product.component.css'
 })
 export class ExplorProductComponent {
+
+
+  protected readonly alert = alert;
+  @Input({required:true}) Title!:string;
+  @Input({required:true}) imageadd1!:string;
+  @Input({required:true}) imageadd2!:string;
+
 
 }
